47ede084dc12d3affc4502909b017d42d1481522
Ahmet Bibi <ahmetbibi@gmail.com>
2025-11-10 13:45:02 +0100

    html entity fix

--- Client/opentriviaapp.ui/src/components/Question.jsx
+++ Client/opentriviaapp.ui/src/components/Question.jsx
@@ -4,7 +4,7 @@ function Question({ question, onAnswer }) {
     return (
         <div className="card mb-3">
             <div className="card-body">
-                <h5 className="card-title">{question.question}</h5>
+                <h5 className="card-title" dangerouslySetInnerHTML={{ __html: question.question }}></h5>
                 {question.options.map((opt, idx) => (
                     <div className="form-check" key={idx}>
                         <input
@@ -14,7 +14,7 @@ function Question({ question, onAnswer }) {
                             value={opt}
                             onChange={() => onAnswer(question.id, opt)}
                         />
-                        <label className="form-check-label">{opt}</label>
+                        <label className="form-check-label" dangerouslySetInnerHTML={{ __html: opt }}></label>
                     </div>
                 ))}
             </div>
